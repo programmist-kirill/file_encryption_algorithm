33c430f68f0a18f41eec0018b1b80f06b8141160
kirill <150036551+programmist-kirill@users.noreply.github.com>
2024-10-28 16:42:32 +0300

    Add files via upload

---
+++ check_eng.py
@@ -0,0 +1,108 @@
+import sys
+
+def check_english(characters):
+    sys.path.append("dictionary/eng/")
+
+    if 'a' in characters:
+        import a
+        a.check_and_write(characters)
+    
+    elif 'b' in characters:
+        import b
+        b.check_and_write(characters)
+    
+    elif 'c' in characters:
+        import c
+        c.check_and_write(characters)
+    
+    elif 'd' in characters:
+        import d
+        d.check_and_write(characters)
+    
+    elif 'e' in characters:
+        import e
+        e.check_and_write(characters)
+
+    elif 'f' in characters:
+        import f
+        f.check_and_write(characters)
+    
+    elif 'g' in characters:
+        import g
+        g.check_and_write(characters)
+    
+    elif 'h' in characters:
+        import h
+        h.check_and_write(characters)
+    
+    elif 'i' in characters:
+        import i
+        i.check_and_write(characters)
+    
+    elif 'j' in characters:
+        import j
+        j.check_and_write(characters)
+
+    elif 'k' in characters:
+        import k
+        k.check_and_write(characters)
+    
+    elif 'l' in characters:
+        import l
+        l.check_and_write(characters)
+    
+    elif 'm' in characters:
+        import m
+        m.check_and_write(characters)
+    
+    elif 'n' in characters:
+        import n
+        n.check_and_write(characters)
+    
+    elif 'o' in characters:
+        import o
+        o.check_and_write(characters)
+    
+    elif 'p' in characters:
+        import p
+        p.check_and_write(characters)
+    
+    elif 'q' in characters:
+        import q
+        q.check_and_write(characters)
+    
+    elif 'r' in characters:
+        import r
+        r.check_and_write(characters)
+    
+    elif 's' in characters:
+        import s
+        s.check_and_write(characters)
+    
+    elif 't' in characters:
+        import t
+        t.check_and_write(characters)
+    
+    elif 'u' in characters:
+        import u
+        u.check_and_write(characters)
+    
+    elif 'v' in characters:
+        import v
+        v.check_and_write(characters)
+    
+    elif 'w' in characters:
+        import w
+        w.check_and_write(characters)
+    
+    elif 'x' in characters:
+        import x
+        x.check_and_write(characters)
+    
+    elif 'y' in characters:
+        import y
+        y.check_and_write(characters)
+    
+    elif 'z' in characters:
+        import z
+        z.check_and_write(characters)
